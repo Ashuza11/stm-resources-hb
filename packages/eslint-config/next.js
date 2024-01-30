3b00218b2030ebf584a6a3173465690f0e97ba84
Alexandre Chambu <amanichambu48@gmail.com>
2024-01-31 01:42:08 +0200

    initial_commit

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -4,32 +4,48 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "eslint-config-turbo",
-  ],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ["only-warn"],
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        require.resolve("@vercel/style-guide/eslint/next"),
+        "eslint-config-turbo",
+        "prettier",
+    ],
+    globals: {
+        React: true,
+        JSX: true,
     },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
+    env: {
+        node: true,
+        browser: true,
+    },
+    plugins: ["only-warn", "prettier"],
+    settings: {
+        "import/resolver": {
+            typescript: {
+                project,
+            },
+        },
+    },
+    ignorePatterns: [
+        // Ignore dotfiles
+        ".*.js",
+        "node_modules/",
+    ],
+    overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
+    rules: {
+        "react-refresh/only-export-components": [
+            "off",
+            { allowConstantExport: true },
+        ],
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-explicit-any": "off",
+        quotes: ["warn", "double"],
+        "jsx-quotes": ["warn", "prefer-double"],
+        semi: ["warn", "always"],
+        "react/prop-types": "off",
+        "react/no-unescaped-entities": "off",
+        "prettier/prettier": "warn",
+        "no-console": "error",
+    },
 };
